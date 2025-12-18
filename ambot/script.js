12a10e5fd70e04beea9935f66927374bb0aceaf3
christianjamesroldan-beep <christianjames.roldan@csucc.edu.ph>
2025-12-18 23:29:12 +0800

    Add portfolio folder

---
+++ ambot/script.js
@@ -0,0 +1,20 @@
+// ================= PAGE PROTECTION =================
+document.addEventListener("DOMContentLoaded", () => {
+    // If NOT logged in → go to login/register page
+    if (localStorage.getItem("isLoggedIn") !== "true") {
+        window.location.href = "../akotosinatoy/index.html";
+    }
+});
+
+// ================= LOGOUT =================
+const logoutBtn = document.getElementById("logout-btn");
+
+if (logoutBtn) {
+    logoutBtn.addEventListener("click", () => {
+        // Remove login session
+        localStorage.removeItem("isLoggedIn");
+
+        // Redirect to login/register page
+        window.location.href = "../akotosinatoy/index.html";
+    });
+}
