b3a024b25c5ba81506a4d5b2c693196f8ce8d00c
christianjamesroldan-beep <christianjames.roldan@csucc.edu.ph>
2025-12-20 00:19:47 +0800

    Update script.js

--- ambot/script.js
+++ ambot/script.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     if (localStorage.getItem("isLoggedIn") !== "true") {
         // Redirect to login page
-        window.location.href = "index.html";
+        window.location.href = "../ambot/index.html";
     }
 });
 
@@ -11,6 +11,6 @@ const logoutBtn = document.getElementById("logout-btn");
 if (logoutBtn) {
     logoutBtn.addEventListener("click", () => {
         localStorage.removeItem("isLoggedIn");
-        window.location.href = "index.html";
+        window.location.href = "../ambot/index.html";
     });
 }
